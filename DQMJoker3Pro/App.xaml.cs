63b20064ceb10ac763b9e86bdbb74b5c60de3b08
turtle-insect <turtle.insect@gmail.com>
2026-03-14 17:40:21 +0900

    Visual Studio 2026 & .NET 10

--- DQMJoker3Pro/App.xaml.cs
+++ DQMJoker3Pro/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Configuration;
 using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace DQMJoker3Pro
@@ -14,4 +10,5 @@ namespace DQMJoker3Pro
 	public partial class App : Application
 	{
 	}
+
 }
